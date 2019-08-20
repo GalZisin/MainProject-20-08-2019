c798b36efe4ea51fae01194d13e7142232795f2d
GalZisin <46171488+GalZisin@users.noreply.github.com>
2019-08-20 19:11:13 +0300

    Add files via upload

---
+++ AirlineManagement/FlightCenterConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineManagement
+{
+    public class FlightCenterConfig
+    {
+        //public const string ADMIN_NAME = "admin";
+        //public const string ADMIN_PASSWORD = "9999";
+
+        public static string strConn = @"Data Source=.;Initial Catalog=AirlineManagementDB;Integrated Security=True";
+        public static int OneDayInterval = 24 * 60 * 60 * 1000; //24 hours in milliseconds
+        
+    }
+}
